3de64ecef5593f0c0ed9fcac265cbec8e79a0ab3
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-03-03 10:32:57 -0500

    commit tree 96f6c08b9deecc430a7ae68e355b24704a38523c

---
+++ lib/insert.sql
@@ -0,0 +1,37 @@
+INSERT INTO users (name, age) VALUES
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26),
+("Eri", 26);
+
+
+INSERT INTO pledges(amount, user_id, project_id) VALUES
+(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2),(100, 1, 2);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test"),
+("TEST", "test",200,"test","Test")
